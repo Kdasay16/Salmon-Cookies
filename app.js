42d2140b850b2047d2be7002f77e498a643d8846
Kayla Asay <kdasay16@gmail.com>
2017-02-16 18:30:06 -0800

    tablegit add .

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 'use strict';
 //store hours
-var openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 var table = document.createElement('table'); //tableEl = <table></table> in HTML
 document.body.appendChild(table);
 
-var storeOne = new CookieStore('first-and-pike', 'First and Pike', 23, 65, 6.3);
-var storeSeaTac = new CookieStore('seatac-airport', 'Seatac Airport', 3, 24, 1.2);
-var storeSeattle = new CookieStore('seattle-center', 'Seattle Center', 11, 38, 3.7);
-var storeCapitol = new CookieStore('capitol-hill', 'Capitol Hill',20, 38, 2.3);
-var storeAlki = new CookieStore('alki', 'Alki', 2, 16, 4.6);
+var header = document.createElement('thead');
+table.appendChild(header);
+
+var headRow = document.createElement('tr');
+header.appendChild(headRow);
+
+var hoursHead = document.createElement('th');
+header.textContent = 'Hours Open';
+header.appendChild(hoursHead);
+
+
+var storeOne = new CookieStore('First and Pike', 23, 65, 6.3);
+var storeSeaTac = new CookieStore('Seatac Airport', 3, 24, 1.2);
+var storeSeattle = new CookieStore('Seattle Center', 11, 38, 3.7);
+var storeCapitol = new CookieStore('Capitol Hill',20, 38, 2.3);
+var storeAlki = new CookieStore('Alki', 2, 16, 4.6);
 
 //stores Array
 var stores = [storeOne, storeSeaTac, storeSeattle, storeCapitol, storeAlki];
 
-function CookieStore(name, displayName, minCustomers, maxCustomers, avgCookies){
-  this.name = name;
+function CookieStore(displayName, minCustomers, maxCustomers, avgCookies){
   this.displayName = displayName;
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgCookies = avgCookies;
   this.hourlyCount = [];
   this.range = maxCustomers - minCustomers;
+  this.totalStoreSales = 0;
 };
 
 CookieStore.prototype.salesPerHour = function(){
@@ -31,7 +42,13 @@ CookieStore.prototype.salesPerHour = function(){
 CookieStore.prototype.populateHourlyCount = function() {
   for (var i = 0; i < openHours.length; i++) {
     this.hourlyCount.push(this.salesPerHour());
+    this.totalStoreSales += this.hourlyCount[i];
   }
+  console.log(this.totalStoreSales);
+  // var totalSales = document.createElement('td');
+  // storeRow.appendChild(totalSales);
+  // storeName.textContent = total;
+
 };
 CookieStore.prototype.makeRow = function() {
   var storeRow = document.createElement('tr');
@@ -46,7 +63,51 @@ CookieStore.prototype.makeRow = function() {
     storeRow.appendChild(hourlyCookieCount);
     hourlyCookieCount.textContent = this.hourlyCount[i];
   }
+  var total = document.createElement('td');
+  storeRow.appendChild(total);
+  total.textContent = this.totalStoreSales;
 };
+
+for (var k = 0; k < openHours.length; k++){
+  var loopHead = document.createElement('th');
+  loopHead.textContent = openHours[k];
+  headRow.appendChild(loopHead);
+}
+
+function hourlyTotals() {
+  var footer = document.createElement('tfoot');
+  var totalName = document.createElement('td');
+  footer.appendChild(totalName);
+  totalName.textContent = 'Hourly Totals';
+  //footer.appendChild(totalName);
+  var totalHourlySales = [];
+
+  for (var i = 0; i < openHours.length; i++) {
+    var hourlyTotal = 0;
+    for (var j = 0; j < stores.length; j++){
+      hourlyTotal += stores[j].hourlyCount[i];
+    }
+    var hourlyTotalTd = document.createElement('td');
+    footer.appendChild(hourlyTotalTd);
+    hourlyTotalTd.textContent = hourlyTotal;
+  }
+  table.appendChild(footer);
+}
+
+//
+//   for (var i = 0; i < openHours.length; i++) {
+//     var hourlyTotal = 0;
+//     for (var j = 0; j < stores.length; j++){
+//       hourlyTotal += stores[j].hourlyCount[i];
+//     }
+//     var hourlyTotalTd = document.createElement('td');
+//     footer.appendChild(hourlyTotalTd);
+//     hourlyTotalTd.textContent = hourlyTotal;
+//   }
+//   table.appendChild(footer);
+// }
+
+
 
 function populateStoreCount() {
   for (var i = 0; i < stores.length; i++){
@@ -61,12 +122,36 @@ function makeAllRows() {
 }
 populateStoreCount();
 makeAllRows();
+hourlyTotals();
+//event listeners
 
+var storeFormEl = document.getElementById('new-store-form');
 
+storeFormEl.addEventListener('submit', handleSubmit); //an event listener just waiting to be fired. Will not fire unless clicked
 
+function handleSubmit(event){
+  event.preventDefault(); //prevented page from reloading after they hit submit, prevents all default behavior for submit
+  event.stopPropagation(); //stops bubbling, stops capturing
 
+  var storeName = event.target.cookieStoreName.value; // target is overarching node, cookieStoreName is the nested node, value will give you the value
+  var minCust = parseInt(event.target.minCustomers.value); // target = storeFormEl, cookieStoreName is the input field, value = text givin
+  var maxCust = parseInt(event.target.maxCustomers.value);
+  var avgCookies = parseFloat(event.target.avgCookies.value);
 
+  console.log(storeName);
+  console.log(minCust);
+  console.log(maxCust);
+  console.log(avgCookies);
 
+  var store = new CookieStore(storeName, minCust, maxCust, avgCookies);
+  store.populateHourlyCount();
+  store.makeRow();
+  stores.push(store);
+
+  console.log(store.hourlyCount);
+
+  console.log('User Pressed Submit Button On Form!');
+}
 // storeOne.populateHourlyCount();
 //storeSeaTac.salesPerHour();
 // storeSeattle.salesPerHour();
@@ -89,9 +174,8 @@ makeAllRows();
 //
 // for (var i = 0; i < openHours.length ; i++){
 //   var saleHour = document.createElement('td');
-//   saleHour.textContent = stores[j].hourlyCount[];
+//   saleHour.textContent = stores[j].hourlyCount;
 //   rowEl.appendChild(sale);
-//   total += stores[j].hourlyCount[j];
 // }
 //
 //
@@ -125,31 +209,3 @@ makeAllRows();
 // }
 //
 //
-//event listeners
-
-var storeFormEl = document.getElementById('new-store-form');
-
-storeFormEl.addEventListener('submit', handleSubmit); //an event listener just waiting to be fired. Will not fire unless clicked
-
-function handleSubmit(event){
-  event.preventDefault(); //prevented page from reloading after they hit submit, prevents all default behavior for submit
-  event.stopPropagation(); //makes sure that this event will not fire off in parent??
-
-  var storeName = event.target.cookieStoreName.value; // target is overarching node, cookieStoreName is the nested node, value will give you the value
-  var minCust = parseInt(event.target.minCustomers.value); // target = storeFormEl, cookieStoreName is the input field, value = text givin
-  var maxCust = parseInt(event.target.maxCustomers.value);
-  var avgCookies = (event.target.avgCookies.value);
-
-  // console.log(storeName);
-  // console.log(minCust);
-  // console.log(maxCust);
-  // console.log(avgCookies);
-  var store = new CookieStore(name, minCust, maxCust, avgCookies);
-
-  stores.push(store);
-
-  console.log(storeName);
-  console.log(store.populateHourlyCount());
-
-  console.log('User Pressed Submit Button On Form!');
-}
